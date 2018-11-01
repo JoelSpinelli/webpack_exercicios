50553a3d6f331d112bf3f155402d5a7ccc18b80e
JoelSpinelli <joel.spinelli@hotmail.com>
2018-10-31 22:47:15 -0300

    Plugin spread no webpack

---
+++ ex.2/index.js
@@ -0,0 +1,3 @@
+import Pessoa from './pessoa'
+const pessoa = new Pessoa('Joel')
+console.log(pessoa.toString())
--- ex/index.js
+++ ex/index.js
@@ -1,3 +1,14 @@
-import Pessoa from './pessoa'
-const pessoa = new Pessoa('Joel')
-console.log(pessoa.toString())
+const produto = {
+  nome: 'Caneta Bic Preta',
+  preco: 1.90,
+  desconto: 0.05
+}
+
+function clone(objeto) {
+  return { ...objeto }
+}
+
+const novoProduto = clone(produto)
+novoProduto.nome = 'Caneta Bic Azul'
+
+console.log(produto, novoProduto)
